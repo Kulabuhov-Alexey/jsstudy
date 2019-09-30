0bed4ffc9c3ba266794b6fb16c3d81daf01d1281
kulabuha <kulabuha@inbox.ru>
2019-09-30 22:27:27 +0300

    dom element task

---
+++ lesson6/src/js/main.js
@@ -0,0 +1,15 @@
+'use strict';
+let start = document.querySelector('#start'),
+    divValue = document.querySelectorAll('div[class*=value]'),
+    expensesInput = document.querySelectorAll('input[class=expenses-item]'),
+    approve = document.querySelectorAll('[class=expenses-item-btn]'),
+    calculate = document.querySelectorAll('[class=count-budget-btn]'),
+    optionalExpenses = document.querySelectorAll('[class=optionalexpenses-item]'),
+    chooseIncome = document.querySelector('.choose-income'),
+    choosePercent = document.querySelector('.choose-percent'),
+    chooseCheckbox = document.querySelector('.checkbox'),
+    chooseSum = document.querySelector('.choose-sum'),
+    dayValue = document.querySelector('.day-value'),
+    yearValue = document.querySelector('.year-value'),
+    monthValue = document.querySelector('.month-value');
+console.log(document.all[1][0]);
